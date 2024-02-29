64d497c76c1869a23346fa0de1c03156c29c4bd6
orekihoutaro <orekihoutaro1218@gmail.com>
2024-02-29 16:22:15 +0800

    added meteor animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        "meteor-effect": "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
+      },
       colors: {
         brandBlue: "#6b58fd",
         brandBlack: "#323232",
