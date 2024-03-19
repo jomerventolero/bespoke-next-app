93853895b567b96ee7a6e4fb10a48e4fbe2572a4
orekihoutaro <orekihoutaro1218@gmail.com>
2024-03-19 11:57:15 +0800

    Fix responsive break points

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import LiveChat from "@/components/LiveChat";
 import MyStaffButtons from "@/components/MyStaffButtons";
 import Navbar from "@/components/Navbar";
 import Testimonials from "@/components/Testimonials";
@@ -14,6 +15,22 @@ export default function Home() {
       <WhyChooseBespoke />
       <Testimonials />
       <Footer />
+      {/*<LiveChat /> */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var service1=document.createElement("script"), service0=document.getElementsByTagName("script")[0];
+              service1.async=true;
+              service1.src='https://embed.tawk.to/643cedfe31ebfa0fe7f8b26b/1gu703ag0';
+              service1.charset='UTF-8';
+              service1.setAttribute('crossorigin','*');
+              service0.parentNode.insertBefore(service1,service0);
+            })();
+          `,
+        }}
+      />
     </main>
   );
 }
